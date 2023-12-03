3efd7c879cc25fb8e2a8dc44da8eb5bdc8bc699b
PandorasActor0 <nelsona2001@hotmail.com>
2023-12-03 12:04:56 -0500

    se configuran las variables .env

--- Backend/app.py
+++ Backend/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = os.environ["MYSQL_HOST"]
+app.config['MYSQL_USER'] = os.environ["MYSQL_USER"]
+app.config['MYSQL_PASSWORD'] = os.environ["MYSQL_PASSORD"]
+app.config['MYSQL_DB'] = os.environ["MESQL_DB"]
+
 conexion = MySQL(app)
+
+mqtt_broker = os.environ["mqtt_broker"]
+mqtt_topic = os.environ["mqtt_topic"]
