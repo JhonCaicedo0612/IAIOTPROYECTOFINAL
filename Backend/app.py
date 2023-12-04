81219e2a0fe605bed7111f0feef798b50516946a
PandorasActor0 <nelsona2001@hotmail.com>
2023-12-04 16:54:23 -0500

    se agrega la funcion añadir usuario

--- Backend/app.py
+++ Backend/app.py
@@ -43,9 +43,31 @@ def on_message(client, userdata, msg):
     with app.app_context():
         cursor = conexion.connection.cursor()
         sql = f"""INSERT INTO datos (idnodo, accx, accy, accz, rotx, roty, rotz, pred, fecha) 
-                  VALUES ({idnodo}, {accx}, {accy}, {accz}, {rotx}, {roty}, {rotz}, '{pred}', NOW())"""
+            VALUES ({idnodo}, {accx}, {accy}, {accz}, {rotx}, {roty}, {rotz}, '{pred}', NOW())"""
         cursor.execute(sql)
         conexion.connection.commit()
+
+@app.route("/")
+def index():
+    return "Welcome"
+
+@app.route("/adduser", methods=['POST'])
+def addUser():
+    try:
+        cursor = conexion.connection.cursor()
+        sql = "SELECT * FROM users WHERE user = '{0}'".format(request.json['user'])
+        cursor.execute(sql)
+        datos = cursor.fetchone()
+        if datos == None:
+            sql="""INSERT INTO users (user,name,password,type)
+            VALUES ('{0}','{1}','{2}',{3})""".format(request.json['user'], request.json['name'],request.json['password'],request.json['type'])
+            cursor.execute(sql)
+            conexion.connection.commit()
+            return jsonify({'mensaje': "Usuario registrado"})
+        else:
+            return jsonify({'mensaje':"El Usuario no esta disponible"})
+    except Exception as ex:
+        return jsonify({'mensaje': 'Error'})
 
 if __name__ == '__main__':
     app.run(debug=True)
